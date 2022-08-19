3a27e432b6e81366058dfe4d9e2b02888141a386
Mihary Joel ANDRIAMILANTO <hei.mihary.2@gmail.com>
2022-08-19 16:35:59 +0300

    feat: "initial project"

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyAk2K1akMNYoUFrYF1iZvCiDeiXjDSBkRE",
+        authDomain: "login-app-5e815.firebaseapp.com",
+        projectId: "login-app-5e815",
+        storageBucket: "login-app-5e815.appspot.com",
+        messagingSenderId: "234014485850",
+        appId: "1:234014485850:web:2fbbd85269b34c53865763"
+    }
+}
+
+export default config;
